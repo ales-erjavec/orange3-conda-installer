cb87e21dc99bca6911cee0e3a8fa1e73efb4c54a
Ales Erjavec <ales.erjavec@fri.uni-lj.si>
2017-03-24 18:29:20 +0100

    Fix recipies

--- orange3-shortcuts/bld.bat
+++ orange3-shortcuts/bld.bat
@@ -4,23 +4,28 @@ if "%RECIPE_DIR%" == "" exit 1
 
 set MENU_DIR=%PREFIX%\Menu
 set ORANGE_ICON=%RECIPE_DIR%\orange.ico
+set ICON_DIR=%PREFIX%\share\orange3\icons
+
 
 if not exist "%MENU_DIR%" (
     mkdir "%MENU_DIR%"
+    if errorlevel 1 exit 1
 )
 
-if errorlevel 1 exit 1
+
 copy "%ORANGE_ICON%" "%MENU_DIR%\orange.ico"
 if errorlevel 1 exit 1
-copy "%RECIPE_DIR%\menu-windows.json" "%MENU_DIR%\orange3-app.json"
+
+copy "%RECIPE_DIR%\menu-windows.json" "%MENU_DIR%\orange3-shortcuts.json"
 if errorlevel 1 exit 1
 
 :: copy (application and .ows file) icon into place
 if not exist "%ICON_DIR%" (
     mkdir "%ICON_DIR%"
 )
-copy "%ORANGE_ICON%" "%ICON_DIR%"
+
+copy "%RECIPE_DIR%\orange.ico" "%ICON_DIR%"
 if errorlevel 1 exit 1
 
-copy "%OWS_ICON%" "%ICON_DIR%"
+copy "%RECIPE_DIR%\OrangeOWS.ico" "%ICON_DIR%"
 if errorlevel 1 exit 1
